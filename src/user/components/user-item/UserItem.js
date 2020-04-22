b394c08ac903c5b0282879a372fa7adfeb8b8bc5
jstonedev <jstonedev521@gmail.com>
2020-04-22 01:12:42 -0400

    Update: display user attributes

--- src/user/components/user-item/UserItem.js
+++ src/user/components/user-item/UserItem.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./userItem.css";
 
-const UserItem = () => {
-	return <div>User Item</div>;
+const UserItem = ({ id, name, image, placeCount }) => {
+	return (
+		<li className="user-item">
+			<div className="user-item__content">
+				<div className="user-item__image">
+					<img src={image} alt={name} />
+				</div>
+				<div className="user-item__info">
+					<h2>{name}</h2>
+					<h3>
+						{placeCount} {placeCount === 1 ? "Place" : "Places"}
+					</h3>
+				</div>
+			</div>
+		</li>
+	);
 };
 
 export default UserItem;
