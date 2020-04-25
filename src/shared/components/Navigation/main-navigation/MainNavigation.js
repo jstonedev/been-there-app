b24aeaf1a214c6a986f304a54890263914982637
jstonedev <jstonedev521@gmail.com>
2020-04-24 22:24:16 -0400

    Create: NavLinks

--- src/shared/components/Navigation/main-navigation/MainNavigation.js
+++ src/shared/components/Navigation/main-navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./mainNavigation.css";
 import { Link } from "react-router-dom";
 import MainHeader from "../main-header/MainHeader";
+import NavLinks from "../nav-links/NavLinks";
 
 const MainNavigation = (props) => {
 	return (
@@ -14,7 +15,9 @@ const MainNavigation = (props) => {
 			<h1 className="main-navigation__title">
 				<Link to="/">YourPlaces</Link>
 			</h1>
-			<nav>...</nav>
+			<nav>
+				<NavLinks />
+			</nav>
 		</MainHeader>
 	);
 };
---
+++ src/shared/components/Navigation/nav-links/NavLinks.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./navLinks.css";
+import { NavLink } from "react-router-dom";
+
+const NavLinks = (props) => {
+	return (
+		<ul className="nav-links">
+			<li>
+				<NavLink to="/" exact>
+					ALL USERS
+				</NavLink>
+			</li>
+			<li>
+				<NavLink to="/:id/places">MY PLACES</NavLink>
+			</li>
+			<li>
+				<NavLink to="/places/new">ADD PLACE</NavLink>
+			</li>
+			<li>
+				<NavLink to="/auth">AUTHENTICATE</NavLink>
+			</li>
+		</ul>
+	);
+};
+
+export default NavLinks;
