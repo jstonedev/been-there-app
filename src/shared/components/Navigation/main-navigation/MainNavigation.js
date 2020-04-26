bb9dc88d342d640937a39872f1c3d9eff370acdb
jstonedev <jstonedev521@gmail.com>
2020-04-26 17:02:12 -0400

    Update: create side-drawer state to manage open and closing of drawer

--- src/shared/components/Navigation/main-navigation/MainNavigation.js
+++ src/shared/components/Navigation/main-navigation/MainNavigation.js
@@ -1,20 +1,34 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./mainNavigation.css";
 import { Link } from "react-router-dom";
 import MainHeader from "../main-header/MainHeader";
 import NavLinks from "../nav-links/NavLinks";
 import SideDrawer from "../side-drawer/SideDrawer";
+import Backdrop from "../../UIElements/backdrop/Backdrop";
 
 const MainNavigation = (props) => {
+	const [drawerOpen, setDrawerOpen] = useState(false);
+
+	const openDrawer = () => {
+		setDrawerOpen(true);
+	};
+
+	const closeDrawer = () => {
+		setDrawerOpen(false);
+	};
+
 	return (
 		<Fragment>
-			<SideDrawer>
-				<nav className="main-navigation__drawer-nav">
-					<NavLinks />
-				</nav>
-			</SideDrawer>
+			{drawerOpen && <Backdrop onClick={closeDrawer} />}
+			{drawerOpen && (
+				<SideDrawer>
+					<nav className="main-navigation__drawer-nav">
+						<NavLinks />
+					</nav>
+				</SideDrawer>
+			)}
 			<MainHeader>
-				<button className="main-navigation__menu-btn">
+				<button className="main-navigation__menu-btn" onClick={openDrawer}>
 					<span />
 					<span />
 					<span />
