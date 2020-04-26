5d6ffff9a91bb456275a0680b09fc0b59a653c30
jstonedev <jstonedev521@gmail.com>
2020-04-26 16:34:09 -0400

    Create: Implement a basic desktop & mobile navigation

--- src/shared/components/Navigation/main-navigation/MainNavigation.js
+++ src/shared/components/Navigation/main-navigation/MainNavigation.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./mainNavigation.css";
 import { Link } from "react-router-dom";
 import MainHeader from "../main-header/MainHeader";
 import NavLinks from "../nav-links/NavLinks";
+import SideDrawer from "../side-drawer/SideDrawer";
 
 const MainNavigation = (props) => {
 	return (
-		<MainHeader>
-			<button className="main-navigation__menu-btn">
-				<span />
-				<span />
-				<span />
-			</button>
-			<h1 className="main-navigation__title">
-				<Link to="/">YourPlaces</Link>
-			</h1>
-			<nav>
-				<NavLinks />
-			</nav>
-		</MainHeader>
+		<Fragment>
+			<SideDrawer>
+				<nav className="main-navigation__drawer-nav">
+					<NavLinks />
+				</nav>
+			</SideDrawer>
+			<MainHeader>
+				<button className="main-navigation__menu-btn">
+					<span />
+					<span />
+					<span />
+				</button>
+				<h1 className="main-navigation__title">
+					<Link to="/">YourPlaces</Link>
+				</h1>
+				<nav className="main-navigation__header-nav">
+					<NavLinks />
+				</nav>
+			</MainHeader>
+		</Fragment>
 	);
 };
 
---
+++ src/shared/components/Navigation/side-drawer/SideDrawer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sideDrawer.css";
+
+const SideDrawer = (props) => {
+	return <aside className="side-drawer">{props.children}</aside>;
+};
+
+export default SideDrawer;
