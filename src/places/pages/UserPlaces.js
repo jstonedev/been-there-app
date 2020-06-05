a3f0956fc6897020ae6f6a8a9dfc4dea0e2acc3a
jstonedev <jstonedev521@gmail.com>
2020-06-04 22:12:06 -0400

    Update: Dynamic segments & route params

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import PlaceList from "../components/place-list/PlaceList";
 
 const DUMMY_PLACES = [
@@ -13,7 +14,7 @@ const DUMMY_PLACES = [
 			lat: 40.7484405,
 			lng: -73.9878584,
 		},
-		creator: "u1",
+		creator: "1",
 	},
 	{
 		id: "2",
@@ -27,12 +28,28 @@ const DUMMY_PLACES = [
 			lat: 40.7484405,
 			lng: -73.9878584,
 		},
-		creator: "u1",
+		creator: "1",
+	},
+	{
+		id: "3",
+		title: "The Colosseum",
+		description:
+			"The Colosseum or Coliseum, also known as the Flavian Amphitheatre, is an oval amphitheatre in the centre of the city of Rome, Italy.",
+		imageUrl:
+			"https://images.pexels.com/photos/2064827/pexels-photo-2064827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		address: "Piazza del Colosseo, 1, 00184 Roma RM, Italy",
+		location: {
+			lat: 41.890209,
+			lng: 12.492231,
+		},
+		creator: "2",
 	},
 ];
 
 const UserPlaces = () => {
-	return <PlaceList items={DUMMY_PLACES} />;
+	const userId = useParams().userId;
+	const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+	return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
